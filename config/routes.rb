ecdf16f539943da12cc29df15fec6e5ea7a9bb60
uk27711 <ec2-user@ip-172-31-14-99.ap-northeast-1.compute.internal>
2025-03-08 09:33:13 +0000

    20250308

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
+  end
+
   root to: 'homes#top'
   get 'homes/about', to: 'homes#about', as: :about
-  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorite, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
